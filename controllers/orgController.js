f9bfaf697e2a01bc000d5eab592e6c96a217da91
John Bell <jdbell123@gmail.com>
2021-05-08 18:35:31 -0400

    updated routes ref in server.js and created findone query for org

--- controllers/orgController.js
+++ controllers/orgController.js
@@ -1,4 +1,4 @@
-const db = require("../models");
+const { Org, User } = require("../models");
 
 // Defining methods for the booksController
 module.exports = {
@@ -11,10 +11,21 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-  findById: function (req, res) {
+  findById: async function (req, res) {
     try {
       console.log(req.body);
-      res.status(200).json(req.body);
+      console.log(req.params.id);
+      const orgData = await Org.findOne({ 
+        where: { id: req.params.id},
+        include: [
+          {
+            model: User,
+            attributes: ['first_name', 'last_name'],
+          },
+        ],
+        });
+      console.log(orgData);
+      res.status(200).json(orgData);
     } catch (err) {
       console.log(err);
       res.status(422).json(err);

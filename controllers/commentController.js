6f070e18a3eb251c8b8c6daa0e3415b476217465
Brandon Haley <kyle7286@gmail.com>
2021-05-09 17:22:24 -0400

    Added API FindByOne logic in comentController; Added Comment model to ticketController so you can get all comments associated with a ticket

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,4 +1,4 @@
-const db = require("../models");
+const { Comment, Ticket, Team, Org, User } = require("../models");
 
 // Defining methods for the booksController
 module.exports = {
@@ -11,10 +11,23 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-  findById: function (req, res) {
+  findById: async function (req, res) {
     try {
       console.log(req.body);
-      res.status(200).json(req.body);
+      const commentData = await Comment.findOne({
+        where: { id: req.params.id },
+        include: [
+          {
+            model: User,
+            attributes: ['first_name', 'last_name'],
+          },
+          {
+            model: Ticket,
+            attributes: ['title', 'Description'],
+          },
+        ],
+      });
+      res.status(200).json(commentData)
     } catch (err) {
       console.log(err);
       res.status(422).json(err);

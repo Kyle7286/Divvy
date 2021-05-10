de6bb591c212fb18a931136b25cdec848e77f7d3
Brandon Haley <kyle7286@gmail.com>
2021-05-10 19:37:56 -0400

    Refactored team model to change a column name

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -2,10 +2,11 @@ const { Team, Org, User } = require("../models");
 
 // Defining methods for the booksController
 module.exports = {
-  findAll: function (req, res) {
+  findAll: async function (req, res) {
     try {
       console.log(req.body);
-      res.status(200).json(req.body);
+      const teamData = await Team.findAll({});
+      res.status(200).json(teamData);
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
@@ -33,7 +34,7 @@ module.exports = {
       res.status(422).json(err);
     }
   },
-  create: function (req, res) {
+  create: async function (req, res) {
     try {
       console.log(req.body);
       res.status(200).json(req.body);
@@ -42,7 +43,7 @@ module.exports = {
       res.status(422).json(err);
     }
   },
-  update: function (req, res) {
+  update: async function (req, res) {
     try {
       console.log(req.body);
       res.status(200).json(req.body);
@@ -51,7 +52,7 @@ module.exports = {
       res.status(422).json(err);
     }
   },
-  remove: function (req, res) {
+  remove: async function (req, res) {
     try {
       console.log(req.body);
       res.status(200).json(req.body);

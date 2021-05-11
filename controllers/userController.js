bf9d6efb10a99a049298e70c9349e599cc96188b
Brandon Haley <kyle7286@gmail.com>
2021-05-10 23:21:02 -0400

    Added update api logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -43,7 +43,13 @@ module.exports = {
   },
   update: async function (req, res) {
     try {
-      res.status(200).json(req.body);
+      const userData = await User.update(req.body, {
+        where: {
+          id: req.params.id
+        }
+      });
+
+      res.status(200).json(userData);
     } catch (err) {
       console.log(err);
       res.status(422).json(err);

0d5abc627cfd5cac9d90c8a16ff919dcc6b34e8c
Brandon Haley <kyle7286@gmail.com>
2021-05-10 22:52:51 -0400

    Added async/await to functions

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,7 +2,7 @@ const { Comment, Ticket, Team, Org, User } = require("../models");
 
 // Defining methods for the booksController
 module.exports = {
-  findAll: function (req, res) {
+  findAll: async function (req, res) {
     try {
       console.log(req.body);
       res.status(200).json(req.body);
@@ -33,7 +33,7 @@ module.exports = {
       res.status(422).json(err);
     }
   },
-  create: function (req, res) {
+  create: async function (req, res) {
     try {
       console.log(req.body);
       res.status(200).json(req.body);
@@ -42,7 +42,7 @@ module.exports = {
       res.status(422).json(err);
     }
   },
-  update: function (req, res) {
+  update: async function (req, res) {
     try {
       console.log(req.body);
       res.status(200).json(req.body);
@@ -51,7 +51,7 @@ module.exports = {
       res.status(422).json(err);
     }
   },
-  remove: function (req, res) {
+  remove: async function (req, res) {
     try {
       console.log(req.body);
       res.status(200).json(req.body);

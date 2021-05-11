aad31a43d3cd9e8702a7e64c5eae701c8def1d85
Brandon Haley <kyle7286@gmail.com>
2021-05-10 23:22:07 -0400

    Completed delete api logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,7 +57,19 @@ module.exports = {
   },
   remove: async function (req, res) {
     try {
-      res.status(200).json(req.body);
+      const userData = await User.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+
+      // If no data found with that ID then return message
+      if (!userData) {
+        res.status(404).json({ message: `Delete not possible. No Team with id ${req.params.id} found in the database!` });
+        return;
+      }
+
+      res.status(200).json(userData);
     } catch (err) {
       console.log(err);
       res.status(422).json(err);

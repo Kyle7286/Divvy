54bc56b629f530c0dab8bb8d61140fd3721ded27
Brandon Haley <kyle7286@gmail.com>
2021-05-10 23:08:16 -0400

    Completed Delete api logic

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -57,7 +57,19 @@ module.exports = {
   },
   remove: async function (req, res) {
     try {
-      res.status(200).json(req.body);
+      const commentData = await Comment.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+
+      // If no data found with that ID then return message
+      if (!commentData) {
+        res.status(404).json({ message: `Delete not possible. No Comment with id ${req.params.id} found in the database!` });
+        return;
+      }
+
+      res.status(200).json(commentData);
     } catch (err) {
       console.log(err);
       res.status(422).json(err);

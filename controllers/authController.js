3779b74ef69cc1652c7c4d1cd50ce3d1f935a09a
John Bell <jdbell123@gmail.com>
2021-05-13 21:52:59 -0400

    added auth/logout route

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ const { User } = require("../models");
 
 // Defining methods for the booksController
 module.exports = {
-    findById: async function (req, res) {
+    login: async function (req, res) {
         try {
             console.log(req.body);
             const userData = await User.findOne({
@@ -28,11 +28,28 @@ module.exports = {
             req.session.save(() => {
                 req.session.user_id = userData.id;
                 req.session.logged_in = true;
-                
+
                 res.json({ user: userData, message: 'You are now logged in!' });
-              });
-       
+            });
+
         } catch (err) {
+            console.log(err);
+            res.status(422).json(err);
+        }
+    },
+
+    logout: async function (req, res) {
+        try {
+            console.log(req.session);
+            if (req.session.logged_in) {
+                req.session.destroy(() => {
+                    res.status(204).end();
+                });
+            } else {
+                res.status(404).end();
+            }
+        }
+        catch (err) {
             console.log(err);
             res.status(422).json(err);
         }
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
 const authController = require("../../controllers/authController");
 
-// Matches with "/api/auth"
+// Matches with "/api/auth/login"
 router.route("/login")
   // .get(orgController.findById)
-  .post(authController.findById);
+  .post(authController.login);
+
+// Matches with "/api/auth/logout"
+router.route("/logout")
+  .post(authController.logout);
 
 // Matches with "/api/org/:id"
 // router

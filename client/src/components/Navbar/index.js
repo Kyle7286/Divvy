4d197a71e7a58a17197603887d1d1df3e73149cb
John Bell <jdbell123@gmail.com>
2021-05-15 14:49:39 -0400

    add code to handle logout click

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,7 +2,7 @@
 /*                             Import Dependencies                            */
 /* -------------------------------------------------------------------------- */
 
-import React from "react";
+import React, { Component } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useMediaQuery } from 'react-responsive';
 
@@ -27,64 +27,64 @@ const Default = ({ children }) => {
 
 /* ------------- Define Main Nav Component (one mobile one not) ------------- */
 
-function Navbar() {
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
 
-    const location = useLocation();
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    }
 
-    return (
-        <>
-            {/*This is what will render below mobile breakpoint for navbar*/}
-            <Mobile>
-                <nav className="navbar bg-dark navbar-expand-lg navbar-dark" data-component="Navbar">
-                    <div>
-                        <button className="navbar-toggler mr-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <a className="navbar-brand mx-auto" href="/">Divvy</a>
-                        <div className="collapse navbar-collapse text-center justify-content-center" id="navbarNav">
-                            <ul className="navbar-nav flex-row justify-content-center">
-                                <li className="nav-item px-0 my-2 text-center mr-4" href="/">
-                                    <Link to="/" className={location.pathname === "/" || location.pathname === "" ? "nav-link active ps-2" : "nav-link ps-2"}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-house-fill" viewBox="0 0 16 16">
-                                            <path fillRule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
-                                            <path fillRule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
-                                        </svg>
-                                        <div>
-                                            Home
+    // const location = useLocation();
+    // console.log("Navbar props: ", props);
+    handleLogoutClick() {
+        console.log("Clicked!");
+        this.props.handleLogout();
+    }
+
+    render() {
+        return (
+            <>
+                {/*This is what will render below mobile breakpoint for navbar*/}
+                < Mobile >
+                    <nav className="navbar bg-dark navbar-expand-lg navbar-dark" data-component="Navbar">
+                        <div>
+                            <button className="navbar-toggler mr-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <a className="navbar-brand mx-auto" href="/">Divvy</a>
+                            <div className="collapse navbar-collapse text-center justify-content-center" id="navbarNav">
+                                <ul className="navbar-nav flex-row justify-content-center">
+                                    <li className="nav-item px-0 my-2 text-center mr-4" href="/">
+                                        <Link to="/" className={location.pathname === "/" || location.pathname === "" ? "nav-link active ps-2" : "nav-link ps-2"}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-house-fill" viewBox="0 0 16 16">
+                                                <path fillRule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
+                                                <path fillRule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
+                                            </svg>
+                                            <div>
+                                                Home
                                             </div>
-                                    </Link>
-                                </li>
-                                <li className="nav-item px-0 my-2 text-center mr-4" href="/dashboard">
-                                    <Link to="/dashboard" className={location.pathname === "/dashboard" || location.pathname === "" ? "nav-link active ps-2" : "nav-link ps-2"}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-speedometer" viewBox="0 0 16 16">
-                                            <path d="M8 2a.5.5 0 0 1 .5.5V4a.5.5 0 0 1-1 0V2.5A.5.5 0 0 1 8 2zM3.732 3.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 8a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 7.31A.91.91 0 1 0 8.85 8.569l3.434-4.297a.389.389 0 0 0-.029-.518z" />
-                                            <path fillRule="evenodd" d="M6.664 15.889A8 8 0 1 1 9.336.11a8 8 0 0 1-2.672 15.78zm-4.665-4.283A11.945 11.945 0 0 1 8 10c2.186 0 4.236.585 6.001 1.606a7 7 0 1 0-12.002 0z" />
-                                        </svg>
-                                        <div>
-                                            Dashboard
+                                        </Link>
+                                    </li>
+                                    <li className=" nav-item my-2 text-cente mr-4">
+                                        <Link to="/team" className={location.pathname === "/team" || location.pathname === "" ? "nav-link active ps-2" : "nav-link ps-2"}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-people-fill" viewBox="0 0 16 16">
+                                                <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                                <path fillRule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z" />
+                                                <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
+                                            </svg>
+                                            <div>
+                                                Team
                                             </div>
-                                    </Link>
-                                </li>
-                                <li className=" nav-item my-2 text-cente mr-4">
-                                    <Link to="/team" className={location.pathname === "/team" || location.pathname === "" ? "nav-link active ps-2" : "nav-link ps-2"}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-people-fill" viewBox="0 0 16 16">
-                                            <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                                            <path fillRule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z" />
-                                            <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
-                                        </svg>
-                                        <div>
-                                            Team
-                                            </div>
-                                    </Link>
-                                </li>
-                                <li className=" nav-item my-2 text-center mr-2">
-                                    <Link to="/profile" className={location.pathname === "/profile" || location.pathname === "" ? "nav-link active ps-2" : "nav-link ps-2"}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-                                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                                            <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                                        </svg>
-                                        <div>
-                                            Profile
+                                        </Link>
+                                    </li>
+                                    <li className=" nav-item my-2 text-center mr-2">
+                                        <Link to="/profile" className={location.pathname === "/profile" || location.pathname === "" ? "nav-link active ps-2" : "nav-link ps-2"}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                                <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                                            </svg>
+                                            <div>
+                                                Profile
                                             </div>
                                     </Link>
                                 </li>
@@ -184,7 +184,7 @@ function Navbar() {
                                     </Link>
                                 </li>
                                 <li className=" nav-item my-2 text-center">
-                                    <Link to="/logout" className={location.pathname === "/logout" || location.pathname === "" ? "nav-link active ps-2" : "nav-link ps-2"}>
+                                    <Link onClick={() => this.handleLogoutClick()} to="/logout" className={location.pathname === "/logout" || location.pathname === "" ? "nav-link active ps-2" : "nav-link ps-2"}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-box-arrow-right" viewBox="0 0 16 16">
                                             <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
                                             <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
@@ -201,6 +201,7 @@ function Navbar() {
             </Default>
         </>
     );
+}
 }
 
 /* -------------------------------------------------------------------------- */

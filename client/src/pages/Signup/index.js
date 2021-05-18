82fc52dd89419609ff13c4fa654b6a985461a837
John Bell <jdbell123@gmail.com>
2021-05-18 18:44:13 -0400

    removed unused code from signup index.js file

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -2,30 +2,11 @@
 /*                             Import Dependencies                            */
 /* -------------------------------------------------------------------------- */
 
-import React, { Component } from "react";
+import React from "react";
 import Container from "../../components/Container";
 import Row from "../../components/Row";
 import Col from "../../components/Column";
 import SignupCard from "../../components/SignupCard";
-import { useMediaQuery } from 'react-responsive';
-import API from "../../utils/API";
-
-
-/* -------------------------------------------------------------------------- */
-/*                           Set Mobile BreakPoints                           */
-/* -------------------------------------------------------------------------- */
-
-// Will render mobile friendly nav, horizontal with diff layout
-const Mobile = ({ children }) => {
-    const isMobile = useMediaQuery({ maxWidth: 1025 })
-    return isMobile ? children : null
-}
-
-// Will render desktop friendly nav, vertical nav
-const Default = ({ children }) => {
-    const isNotMobile = useMediaQuery({ minWidth: 1026 })
-    return isNotMobile ? children : null
-}
 
 /* -------------------------------------------------------------------------- */
 /*                            Define Page Component                           */
@@ -38,37 +19,12 @@ const Default = ({ children }) => {
 */
 function Signup() {
 
-    function handleFormSubmit(event) {
-        event.preventDefault();
-
-        // Collect values from the login form
-        const email = document.querySelector('#email-login').value.trim();
-        const password = document.querySelector('#password-login').value.trim();
-
-        if (email && password) {
-            // Send a POST request to the API endpoint
-            API.login({ email, password })
-                .then(res => {
-                    window.location = '/dashboard';
-                }
-                )
-                .catch(err => {
-                    console.log(err);
-                    document.getElementById("message").innerHTML = err.response.data.message;
-                }
-                )
-        }
-    };
-
         return (
             <div>
-
                 <Container>
                     <Row className="mb-4 d-flex flex-row justify-content-center">
                         <Col>
-                            <SignupCard
-                                handleFormSubmit={handleFormSubmit}
-                            />
+                            <SignupCard />
                         </Col>
                     </Row>
                 </Container>

81968df294fcefe0471af158ab6fa12b58c400b0
John Bell <jdbell123@gmail.com>
2021-05-18 18:45:11 -0400

    added newUser and newOrg functions

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,54 +1,61 @@
 /* -------------------------------------------------------------------------- */
 /*                             Import Dependencies                            */
 /* -------------------------------------------------------------------------- */
-import axios from "axios";
+import axios from "axios";
 /* -------------------------------------------------------------------------- */
 /*                        Define Client Side API Calls                        */
 /* -------------------------------------------------------------------------- */
-export default {
-    /* --------------------------------- tickets -------------------------------- */
+export default {
+    /* --------------------------------- tickets -------------------------------- */
 
-        // Get all tickets
-        getAllTickets: function () {
-            return axios.get("/api/ticket");
-        },
+    // Get all tickets
+    getAllTickets: function () {
+        return axios.get("/api/ticket");
+    },
 
-        // New Tickets
-        newTicket: function(newTicket) {
-            return axios.post("/api/ticket", newTicket);
-        },
+    // New Tickets
+    newTicket: function (newTicket) {
+        return axios.post("/api/ticket", newTicket);
+    },
 
-        // Update Tickets
-        updateTicket: function(id,updatedTicket) {
-            return axios.put("/api/ticket/"+id, updatedTicket);
-        },
+    // Update Tickets
+    updateTicket: function (id, updatedTicket) {
+        return axios.put("/api/ticket/" + id, updatedTicket);
+    },
 
-        // Delete Tickets
-        deleteTicket: function(id) {
-            return axios.delete("api/ticket/"+id);
-        },
+    // Delete Tickets
+    deleteTicket: function (id) {
+        return axios.delete("api/ticket/" + id);
+    },
 
-    /* ---------------------------------- users --------------------------------- */
-        // Get all USERS
-        getAllUsers: function () {
-            return axios.get("/api/user");
-        },
+    /* ---------------------------------- users --------------------------------- */
+    // Get all USERS
+    getAllUsers: function () {
+        return axios.get("/api/user");
+    },
+    newUser: function (newUser) {
+        return axios.post("/api/user", newUser);
+    },
 
-     /* ---------------------------------- clients ------------------------------ */
-        // Get all clients
-        getAllClients: function () {
-            return axios.get("/api/client");
-        },
-    /* -------------------------------- authcheck ------------------------------- */
-        checkAuth: function () {
-            return axios.get("/api/auth/authcheck");
-        },
-    /* ---------------------------------- login --------------------------------- */
-        login: function ({email, password}) {
-            return axios.post("/api/auth/login", {email, password});
-        },
-    /* ---------------------------------- logout --------------------------------- */
-        logout: function () {
-            return axios.post("/api/auth/logout");
-        }
+    /* ---------------------------------- clients ------------------------------ */
+    // Get all clients
+    getAllClients: function () {
+        return axios.get("/api/client");
+    },
+    /* -------------------------------- authcheck ------------------------------- */
+    checkAuth: function () {
+        return axios.get("/api/auth/authcheck");
+    },
+    /* ---------------------------------- login --------------------------------- */
+    login: function ({ email, password }) {
+        return axios.post("/api/auth/login", { email, password });
+    },
+    /* ---------------------------------- logout --------------------------------- */
+    logout: function () {
+        return axios.post("/api/auth/logout");
+    },
+    /* ----------------------------------- orgs ---------------------------------- */
+    newOrg: function (newOrg) {
+        return axios.post("/api/org", newOrg);
+    }
 };

379e1768845a1888284982c36aa0812ab28d0f7c
Brandon Haley <kyle7286@gmail.com>
2021-05-19 00:38:10 -0400

    Refactored how Dashboard.js States are initiatlized; Refactored how props are used to send the state data from dashboard to the sub-components

--- client/src/components/EmployeeCardContainer/index.js
+++ client/src/components/EmployeeCardContainer/index.js
@@ -2,52 +2,91 @@
 /*                             Import Dependencies                            */
 /* -------------------------------------------------------------------------- */
 
-    import React from "react";
-    import EmployeeCard from "../EmployeeCard";
+import React from "react";
+import EmployeeCard from "../EmployeeCard";
+import API from "../../utils/API";
+import { useEffect, useState } from "react";
 
 /* -------------------------------------------------------------------------- */
 /*                              Define Component                              */
 /* -------------------------------------------------------------------------- */
 
-    function EmployeeCardContainer (props) {
-        
+function EmployeeCardContainer(props) {
+
+
+
+
+    // const [user, setUser] = useState([]);
+
+    // // Call when components have loaded
+    // useEffect(() => {
+    //     getUserTeamid(props);
+    // }, [])
+
+    // // Set the user state
+    // function getUserTeamid(x) {
+    //     API.getCurrentUser()
+    //         .then(res => {
+    //             console.log("CURRENT USER", res.data);
+    //             console.log("X", x);
+
+
+    //             const teamEmployees = x.allUsers.filter(user => {
+    //                console.log("FILTERING");
+    //                 return user.team_id === res.data.team_id
+    //             });
+
+    //             console.log("TEAM EMPLOYEES",teamEmployees);
+                
+    //             // setUser(teamEmployees);
+    //         })
+    //         .catch(err => console.log(err));
+    // };
+
+
+    function filterTeamEmployees() {
         // Take all users and filter it to employees
-        const allEmployees = props.allUsers.filter(user=> user.role!="Client");
+        const allEmployees = props.allUsers.filter(user => user.role != "Client");
 
         // Soort employees by their ticket count (due to mounting, make sure its not blank first)
-        if (allEmployees.length!=0) {
-            allEmployees.sort((a,b) => (a.ticketuser.length > b.ticketuser.length ? 1 : -1));
+        if (allEmployees.length != 0) {
+            allEmployees.sort((a, b) => (a.ticketuser.length > b.ticketuser.length ? 1 : -1));
         };
-            
-        // Render the Component by mapping employees and rending sub compoennt in container
-        return (
-           
-            <div className="d-flex flex-wrap justify-content-center">
-                {allEmployees.map((employee,index)=>{
-                    return index < 2 ? (
-                        <EmployeeCard
-                            key = {employee.id}
-                            employeeName={`${employee.first_name} ${employee.last_name}`}
-                            employeeTickets={employee.ticketuser.length ? ("Tickets: " + employee.ticketuser.length) : ("Tickets: 0")}
-                            className="card-body p-1 text-success"
-                        /> 
-                    ): 
-                        <EmployeeCard
-                            key = {employee.id}
-                            employeeName={`${employee.first_name} ${employee.last_name}`}
-                            employeeTickets={employee.ticketuser.length ? ("Tickets: " + employee.ticketuser.length) : ("Tickets: 0")}
-                            className="card-body p-1"
-                        /> 
-                })}
-            </div>
-        );
+
+        return allEmployees;
     }
+
+
+
+    // Render the Component by mapping employees and rending sub compoennt in container
+    return (
+
+        <div className="d-flex flex-wrap justify-content-center">
+            {filterTeamEmployees().map((employee, index) => {
+                return index < 2 ? (
+                    <EmployeeCard
+                        key={employee.id}
+                        employeeName={`${employee.first_name} ${employee.last_name}`}
+                        employeeTickets={employee.ticketuser ? ("Tickets: " + employee.ticketuser.length) : ("Tickets: 0")}
+                        className="card-body p-1 text-success"
+                    />
+                ) :
+                    <EmployeeCard
+                        key={employee.id}
+                        employeeName={`${employee.first_name} ${employee.last_name}`}
+                        employeeTickets={employee.ticketuser ? ("Tickets: " + employee.ticketuser.length) : ("Tickets: 0")}
+                        className="card-body p-1"
+                    />
+            })}
+        </div>
+    );
+}
 
 /* -------------------------------------------------------------------------- */
 /*                              Export Component                              */
 /* -------------------------------------------------------------------------- */
 
-    export default EmployeeCardContainer;
+export default EmployeeCardContainer;
 
 //     {allEmployees.map(employee => (
 //         <EmployeeCard

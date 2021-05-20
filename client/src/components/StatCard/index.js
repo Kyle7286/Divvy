61ec6ab182b973f894590bcecf055b30b8e47739
John Bell <jdbell123@gmail.com>
2021-05-19 21:42:04 -0400

    Made dashboard content clickable - ready for logic to filter

--- client/src/components/StatCard/index.js
+++ client/src/components/StatCard/index.js
@@ -2,26 +2,26 @@
 /*                             Import Dependencies                            */
 /* -------------------------------------------------------------------------- */
 
-    import React from "react";
-    
+import React from "react";
+
 
 /* -------------------------------------------------------------------------- */
 /*                              Define Component                              */
 /* -------------------------------------------------------------------------- */
 
-    function StartCard (props) {
-        return (
-            <div className="card col-3 m-2 text-center float-left px-0" data-component="employeecard">
-                <div className="card-body p-1">
-                    <h6 className="card-text fw-bold">{props.title}</h6>
-                    <p className="card-text">{props.amount}</p>
-                </div>
+function StatCard(props) {
+    return (
+        <div tag="a" style={{ cursor: "pointer" }} className="card col-4 mb-3 text-center float-left px-0" data-component="employeecard">
+            <div className="card-body p-1">
+                <h6 className="card-text fw-bold">{props.title}</h6>
+                <p className="card-text">{props.amount}</p>
             </div>
-        );
-    }
+        </div>
+    );
+}
 
 /* -------------------------------------------------------------------------- */
 /*                              Export Component                              */
 /* -------------------------------------------------------------------------- */
 
-    export default StartCard;
+export default StatCard;

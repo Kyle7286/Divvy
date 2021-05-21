54ef42fced79f6c853870ea7016dad89dea01f27
wryanj <ryanjohnson9685@gmail.com>
2021-05-20 22:01:18 -0400

    merging with johns

--- client/src/components/StatCardContainer/index.js
+++ client/src/components/StatCardContainer/index.js
@@ -4,7 +4,6 @@
 
    
     import React from "react";
-import SectionHeader from "../SectionHeader";
     import StatCard from "../StatCard";
 
 /* -------------------------------------------------------------------------- */
@@ -31,7 +30,6 @@ import SectionHeader from "../SectionHeader";
                 
         // Return the three primary status card components
         return (
-<<<<<<< HEAD
             <div className="bg-light mb-3">
                 <div className="d-flex flex-wrap justify-content-center py-3" data-component="StatCardContainer">
                     <StatCard
@@ -50,24 +48,6 @@ import SectionHeader from "../SectionHeader";
                         handleClick={props.handleClick}
                     />
                 </div>
-=======
-            <div className="d-flex flex-wrap justify-content-center" data-component="StatCardContainer">
-                <StatCard
-                    title="Total"
-                    amount={props.totalTicketCount}
-                    handleClick={props.handleClick}
-                    />
-                <StatCard
-                     title="Open"
-                     amount={props.openTicketCount}
-                     handleClick={props.handleClick}
-                />
-                <StatCard
-                    title="Unassigned"
-                    amount={props.unassignedTicketCount}
-                    handleClick={props.handleClick}
-                />
->>>>>>> 034b512df973c2ff67a92972fa0fe849a3bfacb8
             </div>
            
         );

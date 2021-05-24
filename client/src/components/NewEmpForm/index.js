02145b845df65651c4a7fbf1b3fc4f2b6afe8a8e
wryanj <ryanjohnson9685@gmail.com>
2021-05-23 23:02:18 -0400

    mobile mvp updates still some issues left but not critical for now

--- client/src/components/NewEmpForm/index.js
+++ client/src/components/NewEmpForm/index.js
@@ -21,7 +21,7 @@ function NewEmpForm(props) {
         <Row>
           <Col className="col-lg-6 mx-auto">
             {/* Team Selection */}
-            <div className="mb-3 text-center">
+            <div className="mb-3 mx-2 text-center">
               <label htmlFor="SelectTeam" className="form-label">Select Team</label>
               <select ref={props.refTeamSelected} name="selectList" id="selectList" className="form-select">
                 {props.teams.map((team) => {
@@ -33,7 +33,7 @@ function NewEmpForm(props) {
             </div>
 
             {/* Manager Selection */}
-            <div className="mb-3 text-center">
+            <div className="mb-3 mx-2  text-center">
               <label htmlFor="InputEmpFirstName" className="form-label">Is Manager</label>
               <select ref={props.refIsManager} name="selectList" id="selectList" className="form-select">
                     <option value={false}>No</option>
@@ -42,25 +42,25 @@ function NewEmpForm(props) {
             </div>
 
             {/* Emp First Name */}
-            <div className="mb-3 text-center">
+            <div className="mb-3 mx-2  text-center">
               <label htmlFor="InputEmpFirstName" className="form-label">First Name</label>
               <input ref={props.refEmpFirstName} type="text" className="form-control" id="InputEmpFirstName" aria-describedby="empFirstName" />
             </div>
 
             {/* Emp Last Name */}
-            <div className="mb-3 text-center">
+            <div className="mb-3 mx-2  text-center">
               <label htmlFor="InputEmpLastName" className="form-label">Last Name</label>
               <input ref={props.refEmpLastName} type="text" className="form-control" id="InputEmpLastName" aria-describedby="empLastName" />
             </div>
 
             {/* Email */}
-            <div className="mb-3 text-center">
+            <div className="mb-3 mx-2  text-center">
               <label htmlFor="InputEmail" className="form-label">Email</label>
               <input ref={props.refEmpEmail} type="text" className="form-control" id="InputEmail" aria-describedby="email" />
             </div>
 
             {/* Phone */}
-            <div className="mb-3 text-center">
+            <div className="mb-3 mx-2  text-center">
               <label htmlFor="InputPhone" className="form-label">Phone</label>
               <input ref={props.refEmpPhone} type="text" className="form-control" id="InputPhone" aria-describedby="email" />
             </div>

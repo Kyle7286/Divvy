9359eab787c8b3332fc918d6b5c5db5f85382f46
Brandon Haley <kyle7286@gmail.com>
2021-05-25 22:09:58 -0400

    Cleaned up homepage slightly, adjusted seeds > added rewards for team 2

--- seeds/reward-seeds.js
+++ seeds/reward-seeds.js
@@ -57,6 +57,24 @@ const rewardData = [
         team_id: 1,
         round: 1
     },
+    {
+        reward: 'Candy',
+        req_points: 5,
+        team_id: 2,
+        round: 1
+    },
+    {
+        reward: 'Lunch',
+        req_points: 20,
+        team_id: 2,
+        round: 1
+    },
+    {
+        reward: 'Gift Card (25)',
+        req_points: 40,
+        team_id: 2,
+        round: 1
+    },
 
 ];
 
